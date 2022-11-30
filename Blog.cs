329e24a422acacb9eb1e23882a33aeaa948d1df1
Jacob <jacob.l.hagen@gmail.com>
2022-11-29 19:47:49 -0800

    added all buttons, 1 text field, placeholder functions for phase 3

--- Blog.cs
+++ Blog.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Data;
 
 public partial class Blog : Form {
     private FlowLayoutPanel flowPanel;
@@ -12,13 +13,36 @@ public partial class Blog : Form {
     private TextBox commentTextBox;
     private Button buttonPostComment;
     private DatabaseHandler database;
+    private DataTable dt = new DataTable();
+    private DataGridView dataGridView1 = new DataGridView();
+    //phase 3 features
+    private Button blogList;
+    private TextBox userBlogList;
+    private Button topCommenter;
+    private Button followedBy;
+    private TextBox followedByX;
+    private TextBox followedByY;
+    private Button neverPosted;
+    private Button noNegatives;
+    private Button commonHobby;
+
     public Blog(DatabaseHandler database) {
         this.database = database;
         createGUI();
     }
+    private void blogListOnClick(object sender, EventArgs e) {
+        string usernameToGetBlogsFor = userBlogList.Text;
+        database.getBlogsFromUser(usernameToGetBlogsFor);
+    }
+    private void topCommenterOnClick(object sender, EventArgs e) { }
+    private void followedByOnClick(object sender, EventArgs e) { }
+    private void neverPostedOnClick(object sender, EventArgs e) { }
+    private void noNegativesOnClick(object sender, EventArgs e) { }
+    private void commonHobbyOnClick(object sender, EventArgs e) { }
 
     private void createGUI()
     {
+
         buttonAdd = new Button();
         buttonAdd.Text = "Add Blog";
         buttonAdd.AutoSize = true;
@@ -54,12 +78,62 @@ public partial class Blog : Form {
         buttonPostComment.AutoSize = true;
         buttonPostComment.Click += new System.EventHandler(postCommentOnClick);
 
+        //fill out columns of the datagridview
+        dt.Columns.Add("Username", typeof(string));
+        dt.Columns.Add("Blogs", typeof(string));
+        dt.Columns.Add("Author", typeof(string));
+
+        dt.Rows.Add(new object[] { "Book A", DateTime.Parse("1/1/2016"), "Author A" });
+        dt.Rows.Add(new object[] { "Book B", DateTime.Parse("1/2/2016"), "Author B" });
+        dt.Rows.Add(new object[] { "Book C", DateTime.Parse("1/3/2016"), "Author C" });
+        dataGridView1.DataSource = dt;
+        dataGridView1.Size = new Size(500, 250);
+
+        blogList = new Button();
+        blogList.Text = "List Blogs";
+        blogList.AutoSize = true;
+        blogList.Click += new System.EventHandler(blogListOnClick);
+        userBlogList = new TextBox();
+
+        topCommenter = new Button();
+        topCommenter.Text = "Show Top Commenter";
+        topCommenter.AutoSize = true;
+        topCommenter.Click += new System.EventHandler(topCommenterOnClick);
+
+        followedBy = new Button();
+        followedBy.Text = "Follows";
+        followedBy.AutoSize = true;
+        followedBy.Click += new System.EventHandler(followedByOnClick);
+
+        neverPosted = new Button();
+        neverPosted.Text = "Users Who Haven't Posted";
+        neverPosted.AutoSize = true;
+        neverPosted.Click += new System.EventHandler(neverPostedOnClick);
+
+        noNegatives = new Button();
+        noNegatives.Text = "Users with no Negative Blog comments";
+        noNegatives.AutoSize = true;
+        noNegatives.Click += new System.EventHandler(noNegativesOnClick);
+
+        commonHobby = new Button();
+        commonHobby.Text = "Show Users with Common Hobby";
+        commonHobby.AutoSize = true;
+        commonHobby.Click += new System.EventHandler(commonHobbyOnClick);
+
         flowPanel.Controls.Add(buttonAdd);
         flowPanel.Controls.Add(blogSelectionBox);
         flowPanel.Controls.Add(commentBox);
         flowPanel.Controls.Add(commentText);
         flowPanel.Controls.Add(commentTextBox);
         flowPanel.Controls.Add(buttonPostComment);
+        //add phase 3 buttons to flowpanel
+        flowPanel.Controls.Add(dataGridView1);
+        flowPanel.Controls.Add(blogList);
+        flowPanel.Controls.Add(topCommenter);
+        flowPanel.Controls.Add(followedBy);
+        flowPanel.Controls.Add(neverPosted);
+        flowPanel.Controls.Add(noNegatives);
+        flowPanel.Controls.Add(commonHobby);
 
         flowPanel.Controls.Add(messageBoard);
         Controls.Add(flowPanel);
